45039bbebe10b8e1c05d48dba43f5d0fdf5298a3
Luis Alejandro Pineda Dávila <luisaleja2809@gmail.com>
2026-08-05 00:26:20 -0500

    Actualizar versión a 0.8.1

--- app-release.js
+++ app-release.js
@@ -1,5 +1,5 @@
 (() => {
-  const RELEASE_VERSION = '0.8.0';
+  const RELEASE_VERSION = '0.8.1';
   const previousSidebar = sidebar;
   sidebar = function releaseSidebar(){
     return previousSidebar().replace(/Versión\s+[0-9.]+/g,`Versión ${RELEASE_VERSION}`);
