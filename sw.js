31a397a997a4bcc7bfb1435dc124f44f1eccc348
Luis Alejandro Pineda Dávila <luisaleja2809@gmail.com>
2026-08-04 19:00:31 -0500

    cache supervisor consolidation module offline

--- sw.js
+++ sw.js
@@ -1,25 +1,27 @@
-const CACHE='fenologia-v0.6.0-indexeddb';
+const CACHE='fenologia-v0.7.0-supervisor';
 const ASSETS=[
   './',
   './index.html',
-  './manifest.webmanifest?v=0.6.0',
+  './manifest.webmanifest?v=0.7.0',
   './data/catalogos.json',
-  './app-db.js?v=0.6.0',
-  './app-bootstrap.js?v=0.6.0',
-  './app-core.js?v=0.6.0',
-  './app-eval.js?v=0.6.0',
-  './app-admin.js?v=0.6.0',
-  './app-security.js?v=0.6.0',
-  './app-workflow-patches.js?v=0.6.0',
-  './app-export-filters.js?v=0.6.0',
-  './app-db-ui.js?v=0.6.0',
-  './css-core.css?v=0.6.0',
-  './css-ui.css?v=0.6.0',
-  './css-extra.css?v=0.6.0',
-  './css-workflow-patches.css?v=0.6.0',
-  './css-security.css?v=0.6.0',
-  './css-export-filters.css?v=0.6.0',
-  './css-db.css?v=0.6.0'
+  './app-db.js?v=0.7.0',
+  './app-bootstrap.js?v=0.7.0',
+  './app-core.js?v=0.7.0',
+  './app-eval.js?v=0.7.0',
+  './app-admin.js?v=0.7.0',
+  './app-security.js?v=0.7.0',
+  './app-workflow-patches.js?v=0.7.0',
+  './app-export-filters.js?v=0.7.0',
+  './app-db-ui.js?v=0.7.0',
+  './app-supervisor.js?v=0.7.0',
+  './css-core.css?v=0.7.0',
+  './css-ui.css?v=0.7.0',
+  './css-extra.css?v=0.7.0',
+  './css-workflow-patches.css?v=0.7.0',
+  './css-security.css?v=0.7.0',
+  './css-export-filters.css?v=0.7.0',
+  './css-db.css?v=0.7.0',
+  './css-supervisor.css?v=0.7.0'
 ];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
