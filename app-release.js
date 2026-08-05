702132fb63abb804d3062b90b3375cf4d9920bb3
Luis Alejandro Pineda Dávila <luisaleja2809@gmail.com>
2026-08-05 00:12:53 -0500

    Actualizar identificador de versión a 0.8.0

--- app-release.js
+++ app-release.js
@@ -1,5 +1,5 @@
 (() => {
-  const RELEASE_VERSION = '0.7.5';
+  const RELEASE_VERSION = '0.8.0';
   const previousSidebar = sidebar;
   sidebar = function releaseSidebar(){
     return previousSidebar().replace(/Versión\s+[0-9.]+/g,`Versión ${RELEASE_VERSION}`);
