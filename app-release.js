e99c79931c136fe95fba7a451894d0c98ebd06d2
Luis Alejandro Pineda Dávila <luisaleja2809@gmail.com>
2026-08-05 12:09:58 -0500

    chore: publicar versión 0.12.2

--- app-release.js
+++ app-release.js
@@ -1,5 +1,5 @@
 (() => {
-  const RELEASE_VERSION = '0.12.1';
+  const RELEASE_VERSION = '0.12.2';
   const previousSidebar = sidebar;
   sidebar = function releaseSidebar(){
     return previousSidebar().replace(/Versión\s+[0-9.]+/g,`Versión ${RELEASE_VERSION}`);
