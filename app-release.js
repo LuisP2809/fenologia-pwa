d4902416e03c9fc26df64ba5212e8c1281eb8916
Luis Alejandro Pineda Dávila <luisaleja2809@gmail.com>
2026-08-05 09:34:36 -0500

    Mostrar versión 0.12.1

--- app-release.js
+++ app-release.js
@@ -1,5 +1,5 @@
 (() => {
-  const RELEASE_VERSION = '0.12.0';
+  const RELEASE_VERSION = '0.12.1';
   const previousSidebar = sidebar;
   sidebar = function releaseSidebar(){
     return previousSidebar().replace(/Versión\s+[0-9.]+/g,`Versión ${RELEASE_VERSION}`);
