431f8f8baadff6a58a91ad02fa283fbe0f692699
Luis Alejandro Pineda Dávila <luisaleja2809@gmail.com>
2026-08-11 16:46:27 -0500

    Actualizar versión visible a 0.13.8

--- app-release.js
+++ app-release.js
@@ -1,5 +1,5 @@
 (() => {
-  const RELEASE_VERSION = '0.13.7';
+  const RELEASE_VERSION = '0.13.8';
   const previousSidebar = sidebar;
   sidebar = function releaseSidebar(){
     return previousSidebar().replace(/Versión\s+[0-9.]+/g,`Versión ${RELEASE_VERSION}`);
